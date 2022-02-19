4938b07b9c46d7dbffaf12c42b33f37729b8a639
Martin Šarišský <makosari@gmail.com>
2022-02-19 21:54:27 +0100

    git commit -sign up option

--- main.py
+++ main.py
@@ -87,10 +87,99 @@ def startLog():
     confirmButton = Button(root, text='log in', command= log, width=10)
     confirmButton.place(x=300, y=100)
 
+def startSign():
+
+    for everything in root.winfo_children():
+        everything.destroy()
+
+    #name 
+    name_label = Label(root, text = 'Enter username:')
+    name_label.place(x=20, y=30)
+
+    name_entry = ttk.Entry()
+    name_entry.place(x= 130, y = 30)
+
+
+    #password
+    password_label = Label(root, text = 'Enter password:')
+    password_label.place(x=20, y=70)
+
+    password_entry = ttk.Entry()
+    password_entry.place(x= 130, y = 70)
+
+    #confirm password
+    password_label = Label(root, text = 'Confirm password:')
+    password_label.place(x=20, y=105)
+
+    confirmPassword_entry = ttk.Entry()
+    confirmPassword_entry.place(x= 130, y = 105)
+
+
+    #Signing system
+    def sign():                                  
+        name = name_entry.get()
+        password = password_entry.get()
+        confirmPassword = confirmPassword_entry.get()
+        
+        nameInDatabase = False
+        nameLength = False
+        lineIndex = -1
+
+        f = open("database.txt", "r+")
+        #checking if name is available
+        if len(name) >= 5 :
+            nameLength = True
+            for line in f:
+                lineIndex+=1
+                if name + '~' in line:    #at the end of every name in database
+                    nameInDatabase = True
+                    break
+        else:
+            nameShort_label = Label(root, text = 'Name is too short' + 10*'     ')
+            nameShort_label.place(x=260, y=30)
+
+            #tu
+            blankIt_label = Label(root, text = 20 * '      ')
+            blankIt_label.place(x=20, y=130)
+        
+        if nameInDatabase == True:
+            nameUnavailable_label = Label(root, text = 'Name is already used')
+            nameUnavailable_label.place(x=260, y=30)
+
+            #tu
+            blankIt1_label = Label(root, text = 20 * '      ')
+            blankIt1_label.place(x=20, y=130)
+
+        elif nameInDatabase == False and nameLength == True:
+            blank_label = Label(root, text = 20 * '      ')
+            blank_label.place(x=260, y=30)
+            if len(password) >= 8:
+                if password == confirmPassword:
+                    f.write(f'\n{name}~ {password}')
+                    for everything in root.winfo_children():
+                            everything.destroy()
+                    correctP_label = Label(root, text = 'You are Signed in')
+                    correctP_label.place(x=150, y=50)
+                else:
+                    passwordDontMatch_label = Label(root, text = 'Passwords does not match')
+                    passwordDontMatch_label.place(x=20, y=130)
+            else:
+                    passwordDontMatch_label = Label(root, text = 'Password is too short     ')
+                    passwordDontMatch_label.place(x=20, y=130)
+
+
+        f.close()
+
+        
+
+    #button for log in
+    confirmButton = Button(root, text='Sign up', command= sign, width=10)
+    confirmButton.place(x=300, y=100)
+
 logInButton = Button(root, text='log in', command= startLog, width=10)
 logInButton.place(x=260, y=50)
 
-SignUpButton = Button(root, text='sign up', width=10)
+SignUpButton = Button(root, text='sign up', command=startSign, width=10)
 SignUpButton.place(x=60, y=50)
 
 root.mainloop()
